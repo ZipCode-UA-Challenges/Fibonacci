0e962f33ad8f3ad1c4773961f25ef5fa054dfc1c
Jack Thorp <jackthorp07@gmail.com>
2025-10-02 09:36:30 -0400

    Remove FizzBuzz tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,90 +1,31 @@
+/**
+ * @file test.cpp
+ * 
+ * @brief Test driver for the fibonacci and fibonacciRacer functions.
+ * 
+ * @details The test driver runs and times the fibonacci and fibonacciRacer
+ *          functions for various inputs, and verifies the correctness of their
+ *          outputs.
+ */
+
 #include <chrono>
-#include <functional>
 #include <iostream>
 #include <string>
 #include <sstream>
 
-#include "FizzBuzz.hpp"
+#include "fibonacci.hpp"
 
-template<class T>
-class test
-{
-public:
-    test(const std::string& output, const T& runtime) : output(output), runtime(runtime) { }
+void fibonacciVerifier(std::array<uint64_t, fibonacci::MAX_64_BIT_FIBONACCI_INDEX + 1>& results, int start, int end) {
+    for (int i = start; i <= end; ++i) {
 
-public:
-    const std::string output;
-    const T runtime;
-
-public:
-    friend std::ostream& operator<<(std::ostream& out, const test& object) { return out << object.runtime.count(); }
-    bool operator!=(const test& object) { return output != object.output; }
-};
-
-void controlFizzBuzz(int n)
-{
-    for (int i = 1; i <= n; ++i)
-    {
-        if (i % 15 == 0)
-            std::cout << "FizzBuzz\n";
-        else if (i % 3 == 0)
-            std::cout << "Fizz\n";
-        else if (i % 5 == 0)
-            std::cout << "Buzz\n";
-        else
-            std::cout << i << "\n";
     }
+    std::cout << "All Fibonacci numbers match!" << std::endl;
+
 }
 
-test<std::chrono::microseconds> getOutput(std::function<void(int n)> getFizzBuzz, int n)
-{
-    // Redirect cout to a stringstream
-    std::stringstream output;
-    std::streambuf* oldCout = std::cout.rdbuf(output.rdbuf());
-
-    const auto start = std::chrono::high_resolution_clock::now();
-    getFizzBuzz(n);
-    const auto end = std::chrono::high_resolution_clock::now();
-    const auto durationMicroseconds = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-
-    // Restore original cout
-    std::cout.rdbuf(oldCout);
-
-    return test<std::chrono::microseconds>(output.str(), durationMicroseconds);
-}
-
-int main()
-{
-    const int NUMBER = 10000000;
-
-    const std::function<void(int n)> user = FizzBuzz;
-    auto output = getOutput(user, NUMBER);
-
-    const std::function<void(int n)> control = controlFizzBuzz;
-    auto match = getOutput(control, NUMBER);
-
-    std::cout << "\n\n"
-              << "--------------------------------------------\n\n";
-
-    if (output != match)
-    {
-        std::cout << "\tFizzBuzz test failed :(\n\n"
-                  << "\tOutput does not match expected\n\n"
-                  << "--------------------------------------------\n"
-                  << "\n";
-        return 1;
-    }
-
-    std::cout << "\tFizzBuzz test passed :)\n\n"
-              << "--------------------------------------------\n"
-              << "\n";
-
-    std::cout << "\n\n"
-              << "--------------------------------------------\n\n"
-              << "\tPERFORMANCE INFO:\n\n"
-              << "Program execution time: " << output << " Mircroseconds" << "\n"
-              << "Common answer execution time: " << match << " Microseconds" << "\n\n"
-              << "--------------------------------------------\n";
+int main(int argc, char* argv[]) {
+    std::array<uint64_t, fibonacci::MAX_64_BIT_FIBONACCI_INDEX + 1> results = {0};
+    fibonacciVerifier(results, 0, 0);
 
     return 0;
 }
